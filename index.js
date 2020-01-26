b62a23c78f499adf4454104852c11456e93eb980
wdestein <wdestein@intuitdepecd4d.corp.intuit.net>
2020-01-26 11:35:48 -0800

    More work

--- index.js
+++ index.js
@@ -3,44 +3,80 @@
 	Author Tobias Koppers @sokra
 */
 
-const fs = require("fs");
-const path = require("path");
+// const fs = require("fs");
+// const path = require("path");
 const process = require("process");
 
-function productionLoader(filepath) {
-  console.log('--------- productionLoader() ');
-  return (
-    `try {global.process.dlopen(module, ${JSON.stringify(
-      filepath
-    )}); } catch(e) {` +
-    `throw new Error('node-loader: Cannot open ' + ${JSON.stringify(
-      filepath
-    )} + ': ' + e);}`
-  );
-}
+// function productionLoader(filepath) {
+//   console.log('--------- productionLoader() ');
+//   return (
+//     `try {global.process.dlopen(module, ${JSON.stringify(
+//       filepath
+//     )}); } catch(e) {` +
+//     `throw new Error('node-loader: Cannot open ' + ${JSON.stringify(
+//       filepath
+//     )} + ': ' + e);}`
+//   );
+// }
 
-function developmentLoader(filepath) {
-  console.log('--------- developmentLoader() ');
-  return (
-    `try {global.process.dlopen(module, ${JSON.stringify(
-      filepath
-    )}); } catch(e) {` +
-    `throw new Error('node-loader: Cannot open ' + ${JSON.stringify(
-      filepath
-    )} + ': ' + e);}`
-  );
-}
+// function developmentLoader(filepath) {
+//   console.log('--------- developmentLoader() ');
+//   return (
+//     `try {global.process.dlopen(module, ${JSON.stringify(
+//       filepath
+//     )}); } catch(e) {` +
+//     `throw new Error('node-loader: Cannot open ' + ${JSON.stringify(
+//       filepath
+//     )} + ': ' + e);}`
+//   );
+// }
+
+// function getCode(filepath) {
+//   const jsonFilepath = JSON.stringify(filepath);
+//   const code =
+//     `
+//     const process = require("process");
+//     console.log('----------- loader()');
+//     console.log(JSON.stringify(process.env, null, 4));
+//     console.log('jsonFilepath: ${jsonFilepath}');
+//     try {
+//       global.process.dlopen(module, '${jsonFilepath}');
+//     } catch(e) {
+//       console.log('Error opening ${jsonFilepath}');
+//       console.log(e);
+//       throw new Error(e);
+//     }
+//     `;
+
+//   return code;
+// }
 
 module.exports = function nodeLoader() {
   const filepath = this.resourcePath;
+  const jsonFilepath = JSON.stringify(filepath);
+
   console.log('--------- nodeLoader() ');
   console.log(`__dirname: ${__dirname}`);
   console.log(`filepath: ${filepath}`);
+  console.log(`jsonFilepath: ${jsonFilepath}`);
+
+  const code =
+    `
+    const process = require('process');
+    console.log('----------- loader()');
+    console.log(JSON.stringify(process.env, null, 4));
+    console.log('jsonFilepath: ${jsonFilepath}');
+    try {
+      global.process.dlopen(module, '${jsonFilepath}');
+    } catch(e) {
+      console.log('Error opening ${jsonFilepath}');
+      console.log(e);
+      throw new Error(e);
+    }
+    `;
+
   console.log(JSON.stringify(process.env, null, 4));
-  if (process.env['__TOOLBOX_MODE__'] && process.env['__TOOLBOX_MODE__'] === 'production') {
-    return productionLoader(filepath);
-  } else {
-    return developmentLoader(filepath);
-  }
+  console.log(code);
+  return code;
 }
 
