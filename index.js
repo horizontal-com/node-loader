ad4187d50ca351a3cd7ee7968649a06c0b1c29f9
wdestein <wdestein@intuitdepecd4d.corp.intuit.net>
2020-01-26 03:27:15 -0800

    More work

--- index.js
+++ index.js
@@ -2,13 +2,38 @@
 	MIT License http://www.opensource.org/licenses/mit-license.php
 	Author Tobias Koppers @sokra
 */
+
+const fs = require("fs");
+const path = require("path");
+
 module.exports = function nodeLoader() {
-  return (
-    `try {global.process.dlopen(module, ${JSON.stringify(
-      this.resourcePath
-    )}); } catch(e) {` +
-    `throw new Error('node-loader: Cannot open ' + ${JSON.stringify(
-      this.resourcePath
-    )} + ': ' + e);}`
+  const filename = path.basename(this.resourcePath);
+  const src = fs.readFileSync(this.resourcePath, {encoding: "binary"});
+  const script = `
+const nodeFilename = ${JSON.stringify(filename)};
+const src = ${JSON.stringify(src)};
+require("fs").writeFileSync(nodeFilename, src, "binary");
+try {
+  global.process.dlopen(
+    module,
+    nodeFilename
   );
+} catch(e) {
+  throw new Error('node-loader: Cannot open ' + nodeFilename + ': ' + e);
+}
+`;
+
+  console.log(script);
+  return script;
 };
+
+// module.exports = function nodeLoader() {
+//   return (
+//     `try {global.process.dlopen(module, ${JSON.stringify(
+//       this.resourcePath
+//     )}); } catch(e) {` +
+//     `throw new Error('node-loader: Cannot open ' + ${JSON.stringify(
+//       this.resourcePath
+//     )} + ': ' + e);}`
+//   );
+// };
