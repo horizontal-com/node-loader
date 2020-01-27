15eaf801c85889eece483a1f111bdbaac61ca988
wdestein <wdestein@intuitdepecd4d.corp.intuit.net>
2020-01-26 20:06:40 -0800

    More work

--- index.js
+++ index.js
@@ -32,46 +32,17 @@ const path = require('path');
 
 module.exports = function nodeLoader() {
 
-  console.log('--------- nodeLoader() ');
-  console.log(`__dirname: ${__dirname}`);
-
-  let filepath = this.resourcePath;
-
-  if (path.basename(filepath) === 'pty.node') {
+  console.log(`this.resourcePath: ${this.resourcePath}`);
+  
+  if (path.basename(this.resourcePath) === 'pty.node') {
     if (process.env['__TOOLBOX_MODE__']) {
       if (process.env['__TOOLBOX_MODE__'] == 'production') {
-        filepath = process.env['__TOOLBOX_DIRNAME__'];
-        filepath = path.join(filepath, '..', 'app.asar.unpacked', 'node_modules', 'node-pty', 'build', 'Release', 'pty.node');
+        this.resourcePath = path.join(process.env['__TOOLBOX_DIRNAME__'], '..', 'app.asar.unpacked', 'node_modules', 'node-pty', 'build', 'Release', 'pty.node');
       }
     }
   }
 
   console.log(`this.resourcePath: ${this.resourcePath}`);
-  console.log(`filepath: ${filepath}`);
-
-  const code =
-    `
-    const process = require('process');
-    const filepath = '"${filepath}"';
-
-    console.log('----------- loader()');
-    console.log(filepath);
-    console.log(JSON.stringify(process.env, null, 4));
-
-    try {
-      global.process.dlopen(module, filepath);
-    } catch(e) {
-      console.log('Error opening file ...');
-      console.log(filepath);
-      console.log(e);
-      throw new Error(e);
-    }
-    `;
-
-  console.log(JSON.stringify(process.env, null, 4));
-  console.log(code);
-  // return code;
-
 
   return (
     `try {global.process.dlopen(module, ${JSON.stringify(
