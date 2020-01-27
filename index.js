cb22b332ea5199353b36d7b81f555f03ed209aec
Bill DeStein <billdestein@gmail.com>
2020-01-27 17:45:19 -0600

    More work

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 module.exports = function nodeLoader() {
 
+  console.log('------------- early');
+  console.log(this.resourcePath);
+
   const code = `
     const process = require('process');
     const path = require('path');
     let resourcePath = '${this.resourcePath}';
+
+    console.log('------------ late');
+    console.log(resourcePath);
 
     if (resourcePath.endsWith('pty.node')) {
       if (resourcePath.includes('.build')) {
